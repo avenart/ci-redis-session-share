34e230f934838e3fb0a06ee4d9d0f7c8d2b0b7fc
Avenart Design <info@avenart.com>
2020-04-11 20:28:45 +0100

    Code refactoring

--- app.js
+++ app.js
@@ -7,23 +7,11 @@ const port = process.env.PORT
 const io = require('socket.io')(server, {
   pingTimeout: 60000,
 })
-const redis = require('redis')
-const client = redis.createClient({
-  host: process.env.REDIS_HOST,
-  port: process.env.REDIS_PORT,
-  password: process.env.REDIS_PASS,
-})
-const PHPUnserialize = require('php-unserialize')
 
-const { parseCookie } = require('./utils/cookieParser')
-const { userJoin, userLeave } = require('./utils/users')
+const { authUser, userJoin, userLeave } = require('./utils/users')
 
 server.listen(port, () => {
   console.log(`Server listening on port ${port}`)
-})
-
-client.on('error', (error) => {
-  console.log(error)
 })
 
 io.use((socket, next) => {
@@ -39,21 +27,3 @@ io.use((socket, next) => {
     userLeave(socket)
   })
 })
-
-function authUser(socket) {
-  const cookie = socket.handshake.headers['cookie']
-  const sessionId = parseCookie(cookie)
-  if (sessionId) {
-    client.get(`ci_session:${sessionId}`, (err, res) => {
-      if (err) throw err
-      const userData = PHPUnserialize.unserializeSession(res)
-      if (userData && userData.user_id) {
-        socket.user_id = userData.user_id
-        socket.username = userData.username
-        return true
-      }
-    })
-  } else {
-    return false
-  }
-}
--- utils/users.js
+++ utils/users.js
@@ -1,4 +1,46 @@
+const PHPUnserialize = require('php-unserialize')
+const redis = require('redis')
+const client = redis.createClient({
+  host: process.env.REDIS_HOST,
+  port: process.env.REDIS_PORT,
+  password: process.env.REDIS_PASS,
+})
+
 const users = []
+
+client.on('error', (error) => {
+  console.log(error)
+})
+
+function parseCookie(request) {
+  var result = {}
+  if (request) {
+    request.split(/;\s+/).forEach(function (e) {
+      var parts = e.split(/=/, 2)
+      result[parts[0]] = parts[1] || ''
+    })
+  }
+  sessionId = result['ci_session'] || ''
+  return sessionId
+}
+
+function authUser(socket) {
+  const cookie = socket.handshake.headers['cookie']
+  const sessionId = parseCookie(cookie)
+  if (sessionId) {
+    client.get(`ci_session:${sessionId}`, (err, res) => {
+      if (err) throw err
+      const userData = PHPUnserialize.unserializeSession(res)
+      if (userData && userData.user_id) {
+        socket.user_id = userData.user_id
+        socket.username = userData.username
+        return true
+      }
+    })
+  } else {
+    return false
+  }
+}
 
 function userJoin({ id, user_id, username }) {
   const user = { id, user_id, username }
@@ -14,6 +56,7 @@ function userLeave(socket) {
 }
 
 module.exports = {
+  authUser,
   userJoin,
   userLeave,
 }
